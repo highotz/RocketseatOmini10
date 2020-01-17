4f3f36bcaf21abc0dcce812f6948115a6da5feff
highotz <highotz.hotz@hotmail.com>
2020-01-16 21:11:19 -0300

    starting Web

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+// Componente = funcao que retorna um HTML, CSS, JS | deve ser criado quando o 
+//    codigo se repete ou quando precisamos isolar uma area da aplicacao como um footer;
+//    Bloco isolado de HTML, CSS e JS, o qual nao interfere no restante da aplicacao;
+
+// Propriedades =  informacoes que um componente PAI passa para o componente FILHO
+
+// Estado = informacoes mantidas pelo componente (lembrar: imutabilidade)
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='app'>
+      <aside>
+
+      </aside>
+      <main>
+        
+      </main>
     </div>
   );
 }
